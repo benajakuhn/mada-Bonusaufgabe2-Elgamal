e1909c2227c178d836ddf0fd45e3a2143887c44e
benajakuhn <benaja.kuhn@students.fhnw.ch>
2023-12-03 17:25:23 +0100

    Changes by Andreas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,9 +6,9 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Main {
-
     private static final BigInteger n = new BigInteger("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF", 16);
     private static final BigInteger erzeuger = BigInteger.valueOf(2);
 
@@ -51,7 +51,6 @@ public class Main {
     public static BigInteger getFileBigInt(String fileName) {
         return new BigInteger(getFile(fileName));
     }
-
 
     public static void genPubKey(String skFilname, String pkFilename){
         BigInteger gb = erzeuger.modPow(getFileBigInt(skFilname), n);
@@ -109,6 +108,6 @@ public class Main {
         BigInteger y1_hoch_b = y1.modPow(getFileBigInt(sk), n);
         BigInteger invers = y1_hoch_b.modInverse(n);
 
-        return y2.modPow(invers, n);
+        return y2.multiply(invers).mod(n);
     }
 }
